e41b8ada0d2619fc8b00db269f30ae7ecd1ca079
Anvay Joshi <anvayjoshi1410@gmail.com>
2023-05-18 01:53:27 +0530

    added variable block

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-import { Printstmt, prln } from "./language.js";
+import { Printstmt, Variable } from "./language.js";
 const myButton = document.getElementById("run");
-myButton.addEventListener("click", function(){Runprog()});
-	
+myButton.addEventListener("click", function () { Runprog() });
+
 const myButton1 = document.getElementById("print");
-myButton1.addEventListener("click", function(){CreateBlock()});
+myButton1.addEventListener("click", function () { CreateBlock("printblock") });
+
+const myButton2 = document.getElementById("variable");
+myButton2.addEventListener("click", function () { CreateBlock("varblock") });
 
 var orderofelmnts = [];
+let variables_list = new Object();
+
 // const canvas = document.getElementById("Canvas");
 // canvas.addEventListener("mousedown" , function(event){
 //     const block =  event.target.closest(".Blockheader");
@@ -27,10 +32,10 @@ var orderofelmnts = [];
 // })
 // });
 
-function reorder(elmntarr , orderofexec){
-  for(let i = 0 ; i < orderofexec.length ; i++){
-    for(let j = 0 ; j < elmntarr.length ; j++){
-      if(elmntarr[j].name == orderofexec[i]){
+function reorder(elmntarr, orderofexec) {
+  for (let i = 0; i < orderofexec.length; i++) {
+    for (let j = 0; j < elmntarr.length; j++) {
+      if (elmntarr[j].name == orderofexec[i]) {
         var temp = elmntarr[i];
         elmntarr[i] = elmntarr[j];
         elmntarr[j] = temp;
@@ -40,46 +45,71 @@ function reorder(elmntarr , orderofexec){
   return elmntarr;
 }
 
-function CreateBlock() {
+function CreateBlock(block_type) {
 
-	console.log("init here");
+  console.log("init" + block_type + " here");
+  if (block_type == "printblock") {
     var prnblck = new Printstmt();
     var newBlock = prnblck.create();
-	var parent = document.getElementById("Menu");
+    var parent = document.getElementById("Menu");
     parent.appendChild(newBlock);
-  orderofelmnts.push(prnblck);
-	// dragElement(newBlock);
+    orderofelmnts.push(prnblck);
+    // dragElement(newBlock);
   }
 
-function getElementsinOrder(){
-const parentElement = document.getElementById('Canvas'); // Replace 'parent' with the ID of the parent element
-var elmntarr = [];
-// Get all the children of the parent element
-const children = parentElement.childNodes;
+  else if (block_type == "varblock") {
+    var varblck = new Variable();
+    var newBlock = varblck.create();
+    var parent = document.getElementById("Menu");
+    parent.appendChild(newBlock);
+    orderofelmnts.push(varblck);
 
-// Loop through the children
-for (let i = 0; i < children.length; i++) {
-  const child = children[i];
-  // Filter out non-element nodes (such as text nodes)
-  if (child.nodeType === Node.ELEMENT_NODE) {
-    // Perform actions with each child element
-    elmntarr.push(child.id);
   }
 }
-return elmntarr;
+
+
+function getElementsinOrder() {
+  const parentElement = document.getElementById('Canvas'); // Replace 'parent' with the ID of the parent element
+  var elmntarr = [];
+  // Get all the children of the parent element
+  const children = parentElement.childNodes;
+
+  // Loop through the children
+  for (let i = 0; i < children.length; i++) {
+    const child = children[i];
+    // Filter out non-element nodes (such as text nodes)
+    if (child.nodeType === Node.ELEMENT_NODE) {
+      // Perform actions with each child element
+      elmntarr.push(child.id);
+    }
+  }
+  return elmntarr;
 }
 
 function Runprog() {
-  const terminal  = document.getElementById("Terminal");
-  terminal.textContent='';
-  
+  const terminal = document.getElementById("Terminal");
+  terminal.textContent = '';
+
   const orderofplacement = getElementsinOrder();
-  const orderofexec = reorder(orderofelmnts , orderofplacement);
-  for(let i = 0 ; i < orderofexec.length ; i++){
+  const orderofexec = reorder(orderofelmnts, orderofplacement);
+  for (let i = 0; i < orderofexec.length; i++) {
     const ele = orderofexec[i];
     console.log("running = " + ele.name);
-    ele.display("txt-box" + ele.name);
+    if(ele instanceof Printstmt){
+      ele.display("txt-box" + ele.name);
+    }
+
+    else if(ele instanceof Variable){
+      variables_list[ele.name] = 0;
+    }
+
   }
+
+  console.log("printing variable list for verification");
+  for (const key in variables_list) {
+    console.log(`Key: ${key}, Value: ${variables_list[key]}`);
+  }
+  
   // prln.display();
 }
 
--- language.js
+++ language.js
@@ -1,54 +1,107 @@
 
-export class Printstmt {    
+export class Printstmt {
   static instanceCount = 0;
   constructor() {
     Printstmt.instanceCount++;
     this.name = "Printblock" + Printstmt.instanceCount;
-    console.log("name = "+ this.name);
+    console.log("name = " + this.name);
     console.log("print block initated");
   }
 
-  getname(){
+  getname() {
     console.log(Printstmt.name);
     return Printstmt.name;
   }
 
   create() {
-    this.count += 1;
-    console.log("print no here :" + this.count);
+    //    this.count += 1;
+    //    console.log("print no here :" + this.count);
     console.log("print block created");
     var newBlock = document.createElement("div");
     newBlock.classList.add("Block");
     newBlock.id = "Printblock" + Printstmt.instanceCount;
-    newBlock.setAttribute("draggable" , "true");
+    newBlock.setAttribute("draggable", "true");
     newBlock.style.backgroundColor = "red";
-   
+
     var header = document.createElement("div");
     header.id = "Blockheader" + Printstmt.instanceCount;
     header.textContent = "PRINT";
     newBlock.appendChild(header);
-   
+
     var input = document.createElement("input");
     input.setAttribute("type", "text");
     input.classList.add("Text-Box");
-    input.id =  "txt-box" + this.name;
-    console.log("input id = " + "txt-box" + this.name );
+    input.id = "txt-box" + this.name;
+    console.log("input id = " + "txt-box" + this.name);
     input.setAttribute("placeholder", "Enter text here");
     newBlock.appendChild(input);
-    newBlock.addEventListener('dragstart' , drag);
+    newBlock.addEventListener('dragstart', drag);
     return newBlock;
   }
 
   display(txtboxid) {
-    console.log("text id = "+ txtboxid)
+    console.log("text id = " + txtboxid)
     const txtbox = document.getElementById(txtboxid);
-    const txt = document.createTextNode( txtbox.value + "\n");
+    const txt = document.createTextNode(txtbox.value + "\n");
 
     // console.log("print = "+ txt);
     var terminal = document.getElementById("Terminal");
-   terminal.appendChild(txt );
-   terminal.innerHTML = terminal.innerHTML.replace('\n', '<br>');
+    terminal.appendChild(txt);
+    terminal.innerHTML = terminal.innerHTML.replace('\n', '<br>');
   }
 }
 
-export const prln = new Printstmt();
+export class Variable {
+  static instanceCount = 0;
+  constructor() {
+    Variable.instanceCount++;
+    this.name = "Varblock" + Variable.instanceCount;
+    console.log("name = " + this.name);
+    console.log("variable block initated");
+  }
+
+  getname() {
+    console.log(Variable.name);
+    return Variable.name;
+  }
+
+  create() {
+    //    this.count += 1;
+    //    console.log("print no here :" + this.count);
+    console.log("var block created");
+    var newBlock = document.createElement("div");
+    newBlock.classList.add("Block");
+    newBlock.id = "Varblock" + Variable.instanceCount;
+    newBlock.setAttribute("draggable", "true");
+    newBlock.style.backgroundColor = "green";
+
+    var header = document.createElement("div");
+    header.id = "Blockheader" + Variable.instanceCount;
+    header.textContent = "VAR";
+    newBlock.appendChild(header);
+
+    var input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.classList.add("Text-Box");
+    input.id = "txt-box" + this.name;
+    console.log("input id = " + "txt-box" + this.name);
+    input.setAttribute("placeholder", "var name here");
+    newBlock.appendChild(input);
+    newBlock.addEventListener('dragstart', drag);
+    return newBlock;
+  }
+
+//   display(txtboxid) {
+//     console.log("text id = " + txtboxid)
+//     const txtbox = document.getElementById(txtboxid);
+//     const txt = document.createTextNode(txtbox.value + "\n");
+
+//     // console.log("print = "+ txt);
+//     var terminal = document.getElementById("Terminal");
+//     terminal.appendChild(txt);
+//     terminal.innerHTML = terminal.innerHTML.replace('\n', '<br>');
+//   }
+}
+
+//export const prln = new Printstmt();
+
